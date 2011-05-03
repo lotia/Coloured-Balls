fec60f91a42d89878e3185eb92fed2663dd5e748
Ali Asad Lotia <lotia@umich.edu>
2011-05-04 00:05:25 +0100

    Group 1 03/05/2011 London Clojure Dojo.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :main coloured-balls.core
   :dependencies [[org.clojure/clojure "1.2.0"] [org.clojure/clojure-contrib "1.2.0"]
 		[midje "1.1.1"]
-		[org.clojars.automata/rosado.processing "1.1.0"]])
+		[org.clojars.automata/rosado.processing "1.1.0"]
+                [swank-clojure "1.3.0"]])
--- src/coloured_balls/core.clj
+++ src/coloured_balls/core.clj
@@ -12,21 +12,39 @@
 	(fill (:red ball) (:green ball) (:blue ball))
 	(ellipse (:x ball) (:y ball) (:radius ball) (:radius ball)))
 
-(defn make-ball []
-	{:x (rand-int 400) :y (rand-int 400) :red (rand-int 256) :blue (rand-int 256) :green (rand-int 256) :radius (+ 1 (rand-int 70))})
+(def our-red (rand-int 256))
+(def our-blue (rand-int 256))
+(def our-green (rand-int 256))
+(def our-radius (rand-int 70))
+
+(defn make-ball [x y vel-x vel-y]
+	{:x x :y y :red our-red :blue our-blue :green our-green :radius (+ 1 our-radius) :velocity-x vel-x :velocity-y vel-y})
+
+(def our-ball (make-ball 200 200 2 2))
+
+(defn move-ball [the-ball-were-using]
+  (let [new-vel-x (if (< (:x the-ball-were-using) 400) (:velocity-x the-ball-were-using) (- (:velocity-x the-ball-were-using)))
+        new-vel-y (if (< (:y the-ball-were-using) 400) (:velocity-y the-ball-were-using) (- (:velocity-y the-ball-were-using)))]
+  (make-ball (+ new-vel-x (:x the-ball-were-using)) (+ new-vel-y (:y the-ball-were-using)) new-vel-x new-vel-y
+  )))
+
+(def sequence-of-balls (atom (iterate move-ball our-ball)))
 
 (defn draw
   "Example usage of with-translation and with-rotation."
   []
-	(draw-ball (make-ball))
-  )
+  (background 0 0 0)
+  (draw-ball (first @sequence-of-balls)
+           )
+  (swap! sequence-of-balls rest)
+ )
 
 (defn setup []
   "Runs once."
   (smooth)
   (no-stroke)
   (fill 226)
-  (framerate 10))
+  (framerate 60))
 
 ;; Now we just need to define an applet:
 
